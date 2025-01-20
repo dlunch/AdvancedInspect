6d2e6eb0489149bf536c1cb157da0e4baa0b3375
Inseok Lee <git@dlun.ch>
2025-01-20 16:08:41 +0900

    Initial commit

---
+++ AdvancedInspect/Configuration.cs
@@ -0,0 +1,10 @@
+﻿using Dalamud.Configuration;
+using System;
+
+namespace AdvancedInspect;
+
+[Serializable]
+public class Configuration : IPluginConfiguration
+{
+    public int Version { get; set; } = 0;
+}
---
+++ AdvancedInspect/Plugin.cs
@@ -0,0 +1,35 @@
+﻿using Dalamud.IoC;
+using Dalamud.Plugin;
+using Dalamud.Interface.Windowing;
+using Dalamud.Plugin.Services;
+
+namespace AdvancedInspect;
+
+public sealed class Plugin : IDalamudPlugin
+{
+    [PluginService] internal static IDalamudPluginInterface PluginInterface { get; private set; } = null!;
+    [PluginService] internal static ITextureProvider TextureProvider { get; private set; } = null!;
+    [PluginService] internal static ICommandManager CommandManager { get; private set; } = null!;
+    [PluginService] internal static IClientState ClientState { get; private set; } = null!;
+    [PluginService] internal static IDataManager DataManager { get; private set; } = null!;
+    [PluginService] internal static IPluginLog Log { get; private set; } = null!;
+
+
+    public Configuration Configuration { get; init; }
+
+    public readonly WindowSystem WindowSystem = new("AdvancedInspect");
+
+    public Plugin()
+    {
+        Configuration = PluginInterface.GetPluginConfig() as Configuration ?? new Configuration();
+
+        PluginInterface.UiBuilder.Draw += DrawUI;
+    }
+
+    public void Dispose()
+    {
+        WindowSystem.RemoveAllWindows();
+    }
+
+    private void DrawUI() => WindowSystem.Draw();
+}
